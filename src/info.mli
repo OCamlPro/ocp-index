e6c2bf60f60e89243fe1319e851df6164709213d
Louis Gesbert <louis.gesbert@ocamlpro.com>
2013-03-27 13:15:06 +0100

    Added license and notices

--- src/info.mli
+++ src/info.mli
@@ -1,4 +1,16 @@
-(* *)
+(**************************************************************************)
+(*                                                                        *)
+(*  Copyright 2013 OCamlPro                                               *)
+(*                                                                        *)
+(*  All rights reserved.  This file is distributed under the terms of     *)
+(*  the GNU Public License version 3.0.                                   *)
+(*                                                                        *)
+(*  This software is distributed in the hope that it will be useful,      *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU General Public License for more details.                          *)
+(*                                                                        *)
+(**************************************************************************)
 
 type t
 
