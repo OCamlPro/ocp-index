5ab037d56067d999830afaa2aa1edd1f0e0e1aed
Louis Gesbert <louis.gesbert@ocamlpro.com>
2014-01-21 15:18:30 +0100

    ocp-grep: add options to search in strings within ocaml files

--- src/grepMain.ml
+++ src/grepMain.ml
@@ -12,84 +12,205 @@
 (*                                                                        *)
 (**************************************************************************)
 
-open Approx_tokens
+module Grep : sig
 
-let curpath_update cp tok =
-  let id = function
-    | DOT -> `Dot
-    | UIDENT s -> `Uid s
-    | LIDENT s
-    | INFIXOP0 s | INFIXOP1 s | INFIXOP2 s | INFIXOP3 s | INFIXOP4 s -> `Lid s
-    | AMPERSAND -> `Lid "&"
-    | AMPERAMPER -> `Lid "&&"
-    | BARBAR -> `Lid "||"
-    | COLONEQUAL -> `Lid ":="
-    | MINUS -> `Lid "-"
-    | MINUSDOT -> `Lid "-."
-    | OR -> `Lid "or"
-    | PLUS -> `Lid "+"
-    | PLUSDOT -> `Lid "+."
-    | QUESTIONQUESTION -> `Lid "??"
-    | STAR -> `Lid "*"
-    | BANG -> `Lid "!"
-    | _ -> `None
-  in
-  match (id tok, cp) with
-  | `None, _ -> []
-  | `Dot, `Uid _ :: _ -> `Dot :: cp
-  | `Dot, _ -> []
-  | (`Uid _ | `Lid _) as c, `Dot :: cp -> c :: cp
-  | (`Uid _ | `Lid _) as c, _ -> [c]
-  | _ -> []
+  (** [ident path, filename, input channel, [line, column, length]]
+      Finds occurence of given ident in an opened file.
+      Results are in reverse order. *)
+  val ident: string list -> string -> in_channel -> (int * int * int) list
 
-let path_of_curpath = function
-  | `Dot :: _ -> []
-  | cp -> List.rev_map (function `Uid s | `Lid s -> s | `Dot -> assert false) cp
+  (** [regex, filename, input channel, [line, column, length]]
+      Finds matches of a regexp in the strings of an opened ocaml file.
+      Results are in reverse order. *)
+  val strings_re: Re.re -> string -> in_channel -> (int * int * int) list
 
-let rec list_rm_pfx pfx l = match pfx, l with
-  | px :: pr, x :: r when px = x -> list_rm_pfx pr r
-  | [], l -> Some l
-  | _ -> None
+end = struct
+  open Approx_tokens
 
-(* Returns the list of paths that could match [path] within an env *)
-let rec possible_paths path envs =
-  match envs with
-  | IndexScope.Open p :: envs ->
-      (match list_rm_pfx p path with
-       | Some subpath -> possible_paths subpath envs
-       | None -> []) @
-      possible_paths path envs
-  | IndexScope.Alias (id, p) :: envs ->
-      (match list_rm_pfx p path with
-       | Some subpath -> possible_paths (id :: subpath) envs
-       | None -> []) @
-      possible_paths path envs
-  | [] -> [path]
-
-let grep_file path f ch =
-  let modname =
-    let s = Filename.basename (Filename.chop_extension f) in
-    s.[0] <- Char.uppercase s.[0];
-    s
-  in
-  let f (curpath, lookfor, last_scope, acc) scope tok pos =
-    let lookfor =
-      if scope == last_scope then lookfor
-      else possible_paths path (IndexScope.to_list scope)
+  let curpath_update cp tok =
+    let id = function
+      | DOT -> `Dot
+      | UIDENT s -> `Uid s
+      | LIDENT s
+      | INFIXOP0 s | INFIXOP1 s | INFIXOP2 s | INFIXOP3 s | INFIXOP4 s -> `Lid s
+      | AMPERSAND -> `Lid "&"
+      | AMPERAMPER -> `Lid "&&"
+      | BARBAR -> `Lid "||"
+      | COLONEQUAL -> `Lid ":="
+      | MINUS -> `Lid "-"
+      | MINUSDOT -> `Lid "-."
+      | OR -> `Lid "or"
+      | PLUS -> `Lid "+"
+      | PLUSDOT -> `Lid "+."
+      | QUESTIONQUESTION -> `Lid "??"
+      | STAR -> `Lid "*"
+      | BANG -> `Lid "!"
+      | _ -> `None
     in
-    let curpath = curpath_update curpath tok in
-    if curpath <> [] && List.hd curpath <> `Dot &&
-       List.mem (path_of_curpath curpath) lookfor
-    then
-      curpath, lookfor, scope, pos :: acc
-    else curpath, lookfor, scope, acc
-  in
-  let _, _, _, matches =
-    IndexScope.fold f ([], [], IndexScope.empty, [])
-      ~init:[IndexScope.Open ["Pervasives"]; IndexScope.Open [modname]]
-      ch
-  in
-  matches
+    match (id tok, cp) with
+    | `None, _ -> []
+    | `Dot, `Uid _ :: _ -> `Dot :: cp
+    | `Dot, _ -> []
+    | (`Uid _ | `Lid _) as c, `Dot :: cp -> c :: cp
+    | (`Uid _ | `Lid _) as c, _ -> [c]
+    | _ -> []
+
+  let path_of_curpath = function
+    | `Dot :: _ -> []
+    | cp -> List.rev_map (function `Uid s | `Lid s -> s | `Dot -> assert false) cp
+
+  let rec list_rm_pfx pfx l = match pfx, l with
+    | px :: pr, x :: r when px = x -> list_rm_pfx pr r
+    | [], l -> Some l
+    | _ -> None
+
+  (* Returns the list of paths that could match [path] within an env *)
+  let rec possible_paths path envs =
+    match envs with
+    | IndexScope.Open p :: envs ->
+        (match list_rm_pfx p path with
+         | Some subpath -> possible_paths subpath envs
+         | None -> []) @
+        possible_paths path envs
+    | IndexScope.Alias (id, p) :: envs ->
+        (match list_rm_pfx p path with
+         | Some subpath -> possible_paths (id :: subpath) envs
+         | None -> []) @
+        possible_paths path envs
+    | [] -> [path]
+
+  let ident path f ch =
+    let modname =
+      let s = Filename.basename (Filename.chop_extension f) in
+      s.[0] <- Char.uppercase s.[0];
+      s
+    in
+    let f (curpath, lookfor, last_scope, acc) scope tok pos =
+      let lookfor =
+        if scope == last_scope then lookfor
+        else possible_paths path (IndexScope.to_list scope)
+      in
+      let curpath = curpath_update curpath tok in
+      if curpath <> [] && List.hd curpath <> `Dot &&
+         List.mem (path_of_curpath curpath) lookfor
+      then
+        curpath, lookfor, scope, pos :: acc
+      else curpath, lookfor, scope, acc
+    in
+    let _, _, _, matches =
+      IndexScope.fold f ([], [], IndexScope.empty, [])
+        ~init:[IndexScope.Open ["Pervasives"]; IndexScope.Open [modname]]
+        ch
+    in
+    matches
+
+  let strings_re re _f ch =
+    let line_column str ofs =
+      let rec aux line i =
+        let j = try String.index_from str i '\n' with Not_found -> ofs in
+        if j >= ofs then line, ofs - i
+        else aux (line + 1) (j + 1)
+      in aux 0 0
+    in
+    let match_re s =
+      let rec aux acc pos =
+        let ofs =
+          try Some (Re.get_ofs (Re.exec ~pos re s) 0) with Not_found -> None
+        in
+        match ofs with
+        | None -> acc
+        | Some ((_,pos) as m) -> aux (m :: acc) pos
+      in
+      aux [] 0
+    in
+    let ns = Nstream.of_channel ch in
+    let rec aux ns matches =
+      match Nstream.next ns with
+      | Some ({Nstream.token=STRING _} as tok, ns) ->
+          let s = Lazy.force (tok.Nstream.substr) in
+          let s = String.sub s 1 (String.length s - 2) in (* remove "" *)
+          let pos = Pos.Region.fst tok.Nstream.region in
+          let orig_line, orig_col =
+            Lexing.(pos.pos_lnum, pos.pos_cnum - pos.pos_bol + 1)
+          in
+          let s_matches =
+            List.rev_map (fun (ofs,ofs_end) ->
+                let nl, col = line_column s ofs in
+                let col = if nl = 0 then orig_col + ofs else col in
+                (orig_line + nl, col, ofs_end - ofs)
+              )
+              (match_re s)
+          in
+          aux ns (List.rev_append s_matches matches)
+      | Some (_, ns) -> aux ns matches
+      | None -> matches
+    in
+    aux ns []
+end
+
+module Args = struct
+  open Cmdliner
+
+  let files_of_dir dirs =
+    List.fold_left (fun acc dir ->
+        let files = Array.to_list (Sys.readdir dir) in
+        List.map (Filename.concat dir)
+          (List.filter (fun f ->
+               (Filename.check_suffix f ".ml" ||
+                Filename.check_suffix f ".mli") &&
+               let c = f.[0] in c <> '.' && c <> '#')
+              files)
+        @ acc)
+      [] (IndexMisc.unique_subdirs dirs)
+
+  let pattern =
+    let doc = "Fully qualified ident to search for \
+               (eg. `List.map', `Set.Make.add', ...), \
+               or string with option `-s'." in
+    Arg.(required & pos 0 (some string) None & info [] ~docv:"ID" ~doc)
+
+  let files =
+    let doc = "Files or directories to search into" in
+    let arg = Arg.(value & pos_right 0 file [] & info [] ~docv:"FILES" ~doc) in
+    let get_files = function
+      | [] -> files_of_dir [Filename.current_dir_name]
+      | fs ->
+          let dirs, files = List.partition Sys.is_directory fs in
+          files @ files_of_dir dirs
+    in
+    Term.(pure get_files $ arg)
+
+  let color =
+    let arg =
+      let choices = Arg.enum
+          [ "always", `Always;
+            "never", `Never;
+            "auto", `Auto; ]
+      in
+      let doc =
+        "Colorize the output. $(docv) is either `always', `never' or `auto'."
+      in
+      Arg.(
+        last & opt_all choices [`Auto] & info ["c";"color"] ~docv:"WHEN" ~doc
+      )
+    in
+    let to_bool = function
+      | `Always -> true
+      | `Never -> false
+      | `Auto -> Unix.isatty Unix.stdout
+    in
+    Term.(pure to_bool $ arg)
+
+  let strings =
+    let doc = "Search strings within strings in the source, \
+               instead of searching code"
+    in
+    Arg.(value & flag & info ["s";"strings"] ~doc)
+
+  let regexp =
+    let doc = "Like `--strings', but search a POSIX regular expression." in
+    Arg.(value & flag & info ["e";"regexp-strings"] ~doc)
+end
 
 let rec rmdups = function
   | x::(y::_ as l) when x = y -> rmdups l
@@ -109,92 +230,48 @@ let lines_of_file ch lines =
   seek_in ch 0;
   aux 1 (rmdups lines)
 
-let files_of_dir dirs =
-  List.fold_left (fun acc dir ->
-      let files = Array.to_list (Sys.readdir dir) in
-      List.map (Filename.concat dir)
-        (List.filter (fun f ->
-             Filename.check_suffix f ".ml" ||
-             Filename.check_suffix f ".mli")
-            files)
-      @ acc)
-    [] (IndexMisc.unique_subdirs dirs)
+let print_line color =
+  if not color then
+    fun file _ (l,txt) -> Printf.printf "%s:%d:%s\n" file l txt
+  else
+    fun file matches (l,txt) ->
+      Printf.printf "%s\027[36m:\027[m%d\027[36m:\027[m" file l;
+      let m = List.rev (List.filter (fun (l1,_,_) -> l = l1) matches) in
+      let len = String.length txt in
+      let offs =
+        List.fold_left (fun offs (_,col,toklen) ->
+            print_string (String.sub txt offs (col - offs));
+            print_string "\027[1;31m";
+            print_string (String.sub txt col (min (len - col) toklen));
+            print_string "\027[m";
+            min len (col + toklen)) 0 m
+      in
+      print_endline (String.sub txt offs (String.length txt - offs))
 
-open Cmdliner
+let grep_file finder color file =
+  try
+    let ch = open_in file in
+    let matches = finder file ch in
+    (if matches <> [] then
+       let lines = lines_of_file ch (List.rev_map (fun (l,_,_) -> l) matches) in
+       List.iter (print_line color file matches) lines);
+    close_in ch
+  with Sys_error _ as e ->
+    Printf.eprintf "%s: %s\n%!" file (Printexc.to_string e)
 
-let path =
-  let doc = "Fully qualified ident to search for \
-             (eg. `List.map', `Set.Make.add', ...)" in
-  let arg =
-    Arg.(required & pos 0 (some string) None & info [] ~docv:"ID" ~doc)
-  in
-  let path s = IndexMisc.(key_to_modpath (string_to_key s)) in
-  Term.(pure path $ arg)
-
-let files =
-  let doc = "Files or directories to search into" in
-  let arg = Arg.(value & pos_right 0 file [] & info [] ~docv:"FILES" ~doc) in
-  let get_files = function
-    | [] -> files_of_dir [Filename.current_dir_name]
-    | fs ->
-        let dirs, files = List.partition Sys.is_directory fs in
-        files @ files_of_dir dirs
-  in
-  Term.(pure get_files $ arg)
-
-let color =
-  let arg =
-    let choices = Arg.enum
-        [ "always", `Always;
-          "never", `Never;
-          "auto", `Auto; ]
+let grep pattern files color strings regexp =
+  if strings || regexp then
+    let re =
+      Re_posix.compile
+        (if regexp then Re_posix.re pattern else Re.str pattern)
     in
-    let doc =
-      "Colorize the output. $(docv) is either `always', `never' or `auto'."
-    in
-    Arg.(
-      last & opt_all choices [`Auto] & info ["c";"color"] ~docv:"WHEN" ~doc
-    )
-  in
-  let to_bool = function
-    | `Always -> true
-    | `Never -> false
-    | `Auto -> Unix.isatty Unix.stdout
-  in
-  Term.(pure to_bool $ arg)
-
-let grep path files color =
-  let print_line =
-    if not color then
-      fun file _ (l,txt) -> Printf.printf "%s:%d:%s\n" file l txt
-    else
-      fun file matches (l,txt) ->
-        Printf.printf "%s\027[36m:\027[m%d\027[36m:\027[m" file l;
-        let m = List.rev (List.filter (fun (l1,_,_) -> l = l1) matches) in
-        let len = String.length txt in
-        let offs =
-          List.fold_left (fun offs (_,col,toklen) ->
-              print_string (String.sub txt offs (col - offs));
-              print_string "\027[1;31m";
-              print_string (String.sub txt col (min (len - col) toklen));
-              print_string "\027[m";
-              min len (col + toklen)) 0 m
-        in
-        print_endline (String.sub txt offs (String.length txt - offs))
-  in
-  List.iter (fun file ->
-      try
-        let ch = open_in file in
-        let matches = grep_file path file ch in
-        let lines = lines_of_file ch (List.rev_map (fun (l,_,_) -> l) matches) in
-        List.iter (print_line file matches) lines;
-        close_in ch
-      with e ->
-        Printf.eprintf "%s: %s\n%!" file (Printexc.to_string e)
-    )
-    files
+    List.iter (grep_file (Grep.strings_re re) color) files
+  else
+    let path = IndexMisc.(key_to_modpath (string_to_key pattern)) in
+    List.iter (grep_file (Grep.ident path) color) files
 
 let () =
+  let open Cmdliner in
   let doc = "Locates instances of a given OCaml ident in source files, \
              handling (local) opens, module, etc." in
   let man = [
@@ -205,7 +282,8 @@ let () =
   in
   match
     Term.eval
-      (Term.(pure grep $ path $ files $ color),
+      (Term.(pure grep
+             $ Args.pattern $ Args.files $ Args.color $ Args.strings $ Args.regexp),
        Term.info "ocp-grep" ~version:"0.1" ~doc ~man)
   with
   | `Error _ -> exit 1
