a1b0ab0a020bf2e3b5eec57f762e397be569406e
Louis Gesbert <louis.gesbert@ocamlpro.com>
2015-06-22 11:34:17 +0900

    Version 1.1.2

--- src/grepMain.ml
+++ src/grepMain.ml
@@ -310,7 +310,7 @@ let () =
     Term.eval
       (Term.(pure grep
              $ Args.pattern $ Args.files $ Args.color $ Args.strings $ Args.regexp),
-       Term.info "ocp-grep" ~version:"1.1.1" ~doc ~man)
+       Term.info "ocp-grep" ~version:"1.1.2" ~doc ~man)
   with
   | `Ok true -> exit 0
   | `Ok false -> exit 1
