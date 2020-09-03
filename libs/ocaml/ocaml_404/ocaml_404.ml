c7dc385586a14ce2ea07124ce3da9b4b7bf943cf
Louis Gesbert <louis.gesbert@ocamlpro.com>
2020-09-03 11:22:58 +0200

    Add all supported OCaml ASTs

---
+++ libs/ocaml/ocaml_404/ocaml_404.ml
@@ -0,0 +1,17 @@
+open Ocaml___
+
+module Open_me = struct
+  module Ident = Ident
+  module Path = Path
+  module Types = Types
+  module Ctype = Ctype
+  module Parsetree = Parsetree
+  module Typedtree = Typedtree
+  module Cmi_format = Cmi_format
+  module Cmt_format = Cmt_format
+  module Printtyp = Printtyp
+end
+
+module Outcometree = Outcometree
+module Lexing = Lexing
+module Location = Location
