805bfddd91abf6d36e1d64acb14d2202c2ca6a8f
Drup <drupyog@zoho.com>
2022-05-18 12:07:39 +0200

    Reenable Naming contexts.
    The numbering issues should be gone thanks to more prefixing.

--- libs/indexBuild.ml
+++ libs/indexBuild.ml
@@ -27,9 +27,6 @@ open IndexTypes
 *)
 type parents = (string list * t Lazy.t) list
 
-(* Disable the cleverness from the compiler's pretty printers *)
-let () = Printtyp.Naming_context.enable false
-
 (* - Utility functions - *)
 
 open IndexMisc
